059c3febbba8629dfeef954e07f6abfa25912b4f
Infi-7 <83002420+Infi-7@users.noreply.github.com>
2023-02-10 20:57:44 +0530

    Add files via upload
    main.py update as per streamlit

--- API/main.py
+++ API/main.py
@@ -1,43 +1,42 @@
-from fastapi import FastAPI, UploadFile, File
-import uvicorn
-import numpy as np
 from io import BytesIO
+import streamlit as st
+import numpy as np
 from PIL import Image
 import tensorflow as tf
 
-app = FastAPI()
+list=['png','jpg','jpeg']
 
-MODEl = tf.keras.models.load_model("../models/1")
-CLASS_NAMES = ["Early blight", "Late Blight","Healthy"]
+MODEL = tf.keras.models.load_model("../models/1")
 
-@app.get("/ping")
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
-async def ping():
-    return "Hello, I am active"
+predicted_class = None
 
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
     return image
 
-@app.post("/predict")
-async def predict(
-    file: UploadFile = File(...)
 
-): 
-    image = read_file_as_image(await file.read())
+file = st.file_uploader('Upload a PNG image', type=list)
+show_file = st.empty()
+if not file:
+    show_file.info("Please upload a file of type: " + ", ".join(["csv", "png", "jpg"]))
+show_file.image(file)
+
+def main():
+    
+    image = read_file_as_image(file.read())
     img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
 
-    prediction = MODEl.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
 
-    predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
-    confidence = np.max(prediction[0])
+    if st.button('Predict'):
+        st.write('Disease :',predicted_class)
+        st.write('Accuracy :',("%.0f%%" % (100 * confidence)))
 
-    return {
-        'class':predicted_class,
-        'confidence':float(confidence)
-
-    }
-
-
-if __name__=="__main__":
-    uvicorn.run(app,host='localhost', port=8000)
+    
+if __name__ == "__main__":
+    main()

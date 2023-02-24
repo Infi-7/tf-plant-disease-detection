e02f26763871eea34cf6af997c1e9ad979220ddb
Infi-7 <83002420+Infi-7@users.noreply.github.com>
2023-02-24 19:09:59 +0530

    features update

--- API/main.py
+++ API/main.py
@@ -5,12 +5,16 @@ from PIL import Image
 import tensorflow as tf
 import pickle
 import numpy as np
+import csv
+from datetime import date
+import pandas as pd
+
 
 tab1, tab2, tab3 = st.tabs(["Crop Recommedations", "Disease Predictions", "Fertilizer Recommendations"])
 
-rf_m = open("../models/rf_model.pkl","rb")
-knn_m = open("../models/rf_model.pkl","rb")
-svm_m = open("../models/rf_model.pkl","rb")
+rf_m = open("../models/crop_rec/rf_model.pkl","rb")
+knn_m = open("../models/crop_rec/knn_model.pkl","rb")
+svm_m = open("../models/crop_rec/svm_model.pkl","rb")
 
 
 rf = pickle.load(rf_m)
@@ -19,9 +23,26 @@ svm = pickle.load(svm_m)
 
 list=['png','jpg','jpeg']
 
-MODEL = tf.keras.models.load_model("../models/1")
+MODEL = tf.keras.models.load_model("../models/disease_pred/1")
 
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+today = date.today()
+
+
+CLASS_NAMES = ['Pepper__bell___Bacterial_spot',
+ 'Pepper__bell___healthy',
+ 'Potato___Early_blight',
+ 'Potato___Late_blight',
+ 'Potato___healthy',
+ 'Tomato_Bacterial_spot',
+ 'Tomato_Early_blight',
+ 'Tomato_Late_blight',
+ 'Tomato_Leaf_Mold',
+ 'Tomato_Septoria_leaf_spot',
+ 'Tomato_Spider_mites_Two_spotted_spider_mite',
+ 'Tomato__Target_Spot',
+ 'Tomato__Tomato_YellowLeaf__Curl_Virus',
+ 'Tomato__Tomato_mosaic_virus',
+ 'Tomato_healthy']
 
 
 with tab1:
@@ -60,17 +81,14 @@ with tab2:
     file = st.file_uploader('Upload a leaf image', type=list)
     show_file = st.empty()
     if not file:
-        show_file.info("Please upload a file of type: " + ", ".join(["csv", "png", "jpg"]))
-    
+        show_file.info("Please upload a file of type: " + ", ".join(["jpeg", "png", "jpg"]))
     
     else:
         show_file.image(file)
 
-
     def read_file_as_image(file) -> np.ndarray:
         image = np.array(Image.open(BytesIO(file)))
         return image
-
 
     def main():
 
@@ -84,14 +102,84 @@ with tab2:
 
             confidence = np.max(predictions[0])
 
-            st.write('Disease :',predicted_class)
+            print(predicted_class)
+
+            if predicted_class == "Pepper__bell___Bacterial_spot":
+                crop = "Bell Pepper"
+                c_disease = "Bacterial spot"
+                    
+            elif predicted_class == "Pepper__bell___healthy":
+                crop = "Bell Pepper"
+                c_disease = "Healthy"
+        
+            elif predicted_class == 'Potato___Early_blight':
+                crop = "Potato"
+                c_disease = 'Early Blight'
+            elif predicted_class == 'Potato___Late_blight':
+                crop = "Potato"
+                c_disease = "Late Blight"
+            elif predicted_class == 'Potato___healthy':
+                crop = "Potato"
+                c_disease = "Healthy"
+            
+            elif predicted_class == 'Tomato_Bacterial_spot':
+                crop = "Tomato"
+                c_disease = 'Bacterial spot'
+            elif predicted_class == 'Tomato_Early_blight':
+                crop = "Tomato"
+                c_disease = 'Early blight'                
+            elif predicted_class == 'Tomato_Late_blight':
+                crop = "Tomato"
+                c_disease = 'Late blight'
+            elif predicted_class == 'Tomato_Leaf_Mold':
+                crop = "Tomato"
+                c_disease = 'Leaf Mold'
+            elif predicted_class == 'Tomato_Septoria_leaf_spot':
+                crop = "Tomato"
+                c_disease = 'Septoria leaf spot'
+            elif predicted_class == 'Tomato_Spider_mites_Two_spotted_spider_mite':
+                crop = "Tomato"
+                c_disease = 'Spider mites Two spotted spider mite'
+            elif predicted_class == 'Tomato__Target_Spot':
+                crop = "Tomato"
+                c_disease = 'Target Spot'
+            elif predicted_class == 'Tomato__Tomato_YellowLeaf__Curl_Virus':
+                crop = "Tomato"
+                c_disease = 'YellowLeaf Curl Virus'
+            elif predicted_class == 'Tomato__Tomato_mosaic_virus':
+                crop = "Tomato"
+                c_disease = 'Mosaic virus'
+            elif predicted_class == 'Tomato_healthy':
+                crop = "Tomato"
+                c_disease = 'Healthy'
+
+            
+            st.write('Crop :',crop)
+            st.write('Disease :',c_disease)
             st.write('Accuracy :',("%.0f%%" % (100 * confidence)))
 
+
+            
+            column_name = ["Crop","Disease","Date","Year","Month","Day"]
+            data = [crop,c_disease,today,today.year,today.month,today.day]
+
+            with open('../datasets/results.csv', 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow(data)
+                f.close()
+        
+        graph = pd.read_csv("../datasets/results.csv")
+        y_plot = graph["Crop"]
+        x_plot = graph["Date"]
+
+        if st.button("Visualize"):
+            chart_data = pd.DataFrame(x_plot,y_plot)
+
+            st.line_chart(chart_data)
             
     if __name__ == "__main__":
         main()
 
 with tab3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
-
+   st.header("hello")
+   

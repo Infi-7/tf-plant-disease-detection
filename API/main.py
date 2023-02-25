dbf185a5bae98574d4f19825a7d013164ff2324e
Infi-7 <83002420+Infi-7@users.noreply.github.com>
2023-02-25 18:01:53 +0530

    final changes

--- API/main.py
+++ API/main.py
@@ -8,9 +8,10 @@ import numpy as np
 import csv
 from datetime import date
 import pandas as pd
-import matplotlib.pyplot as plt
+import mysql.connector
+from sklearn import preprocessing
 
-
+mydb = mysql.connector.connect(host="localhost",user="root",password="root",database="mydatabase")
 
 tab1, tab2, tab3 = st.tabs(["Crop Recommedations", "Disease Predictions", "Fertilizer Recommendations"])
 
@@ -18,12 +19,20 @@ rf_m = open("../models/crop_rec/rf_model.pkl","rb")
 knn_m = open("../models/crop_rec/knn_model.pkl","rb")
 svm_m = open("../models/crop_rec/svm_model.pkl","rb")
 
+f_m  = open("../models/f_rec/fertilizer_model.pkl","rb")
+
 
 rf = pickle.load(rf_m)
 knn = pickle.load(knn_m)
 svm = pickle.load(svm_m)
+f = pickle.load(f_m)
 
 list=['png','jpg','jpeg']
+
+f_df = pd.read_csv("../datasets/Fertilizer_Prediction.csv")
+label_encoder = preprocessing.LabelEncoder()
+f_df['Soil Type']= label_encoder.fit_transform(f_df['Soil Type'])
+f_df['Crop Type']= label_encoder.fit_transform(f_df['Crop Type'])
 
 MODEL = tf.keras.models.load_model("../models/disease_pred/1")
 
@@ -175,30 +184,65 @@ with tab2:
         x_plot = graph["Date"]
 
         if st.button("Visualize"):
-            plot = pd.read_csv("../datasets/results.csv")
-            c_plot=plot
-            df = c_plot.drop(['Disease','Year','Month','Day'],axis=1)
+            chart_data = pd.DataFrame(x_plot,y_plot)
 
-            df_t = df[df['Crop'] == 'Tomato']
-            plot_t = df_t.value_counts().sum()
-
-            df_p = df[df['Crop'] == 'Potato']
-            plot_p = df_p.value_counts().sum()
-
-            df_b = df[df['Crop'] == 'Bell Pepper']
-            plot_b = df_b.value_counts().sum()
-
-            plt.rcParams.update({'font.size': 4})
-
-            fig, ax = plt.subplots(figsize=(2, 2))
-            y = np.array([plot_p,plot_t,plot_b])
-
-            plt.pie(y, autopct='%1.2f%%')
-            st.pyplot(fig) 
+            st.line_chart(chart_data)
             
     if __name__ == "__main__":
         main()
 
 with tab3:
-   st.header("hello")
+    option_m = st.selectbox('Select desired choice: ',('Disease', 'Overall'))
    
+    if option_m == "Disease":
+        option_crop = st.selectbox('Select a crop',("Tomato","Potato","Bell Pepper"))
+
+        if option_crop == "Bell Pepper":
+            option_dis =  st.selectbox("Select a disease:",("Bacterial spot",""))
+        elif option_crop == "Potato":
+            option_dis = st.selectbox("Select a disease:",('Early blight','Late blight'))
+
+        elif option_crop == "Tomato":
+            option_dis = st.selectbox("Select a disease:",('Bacterial spot','Early blight','Late blight','Leaf Mold','Septoria leaf spot','Spider mites Two spotted spider mite','Target Spot','YellowLeaf Curl Virus','mosaic virus',))
+
+        mycursor = mydb.cursor()
+
+        sql = "SELECT remedy FROM fertilizers WHERE crop = %s AND disease =  %s"
+        adr = (option_crop, option_dis, )
+
+        mycursor.execute(sql, adr)
+
+        myresult = mycursor.fetchall()
+
+        if st.button("Show results"):
+            st.write("The fertilizers for {} with {} disease are as follows: ".format(option_crop,option_dis))
+            for c in  myresult:
+                st.write(c[0],"\n")
+
+
+    elif option_m == "Overall":
+
+            temp1 = st.text_input("Enter Temperature","")
+            h1 = st.text_input("Enter Humidity","")
+            m1 = st.text_input("Enter Moisture","")
+            s1 = st.selectbox("Enter soil type",('Sandy', 'Loamy', 'Black', 'Red', 'Clayey'))
+            c1 = st.selectbox("Enter Crop type",('Maize', 'Sugarcane', 'Cotton', 'Tobacco', 'Paddy', 'Barley','Wheat', 'Millets', 'Oil seeds', 'Pulses', 'Ground Nuts'))
+            n1 = st.text_input("Enter N","")
+            p1 = st.text_input("Enter P","")
+            k1 = st.text_input("Enter K","")
+
+            dict1 = {'Sandy':'4', 'Loamy':'2', 'Black':'0', 'Red':'3', 'Clayey':'1'}
+            dict2 = {'Maize':'3', 'Sugarcane':'8', 'Cotton':'1', 'Tobacco':'9', 'Paddy':'6', 'Barley':'0',
+       'Wheat':'10', 'Millets':'4', 'Oil seeds':'5', 'Pulses':'7', 'Ground Nuts':'2'}
+            
+            choice_1 = float(dict1["{}".format(s1)])
+            print(type(choice_1))
+            choice_2 = float(dict2["{}".format(c1)])
+
+            if st.button("Display Predictions"):
+                custom = np.array([[temp1,h1,m1,choice_1,choice_2,n1,p1,k1]])
+                results_1 = f.predict(custom)
+                
+
+                for final in results_1:
+                    st.write(final)
